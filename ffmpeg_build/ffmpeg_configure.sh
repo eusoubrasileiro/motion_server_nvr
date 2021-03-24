ace17bb9b9b168573c8bfdc70aebab6c5ad0bdb2
André <eusoubrasileiro@gmail.com>
2021-03-24 13:22:47 -0300

    migrating to linux-deploy debian step2

--- ffmpeg_build/ffmpeg_configure.sh
+++ ffmpeg_build/ffmpeg_configure.sh
@@ -1,33 +1,3 @@
-pkg install freetype libass cmake libgnutls gnutls libopus libdav1d libmp3lame libvorbis libvpx pkg-config wget nasm libx264 libx265 libxcb ninja libtool
-
-cd ffmpeg
-./configure \
---prefix="$HOME/ffmpeg_build" \
---pkg-config-flags="--static" \
---extra-cflags="-I$HOME/ffmpeg_sources/ffmpeg/include" \
---extra-ldflags="-L$HOME/ffmpeg_sources/ffmpeg/lib" \
---extra-libs="-lpthread -lm" \
---bindir="$HOME/bin" \
---enable-gpl \
---enable-gnutls  \
---enable-libass  \
---enable-libfreetype \
---enable-libmp3lame   \
---enable-libvorbis \
---enable-libvpx \
---enable-libx264 \
---enable-libx265  \
---enable-nonfree \
---enable-cross-compile \
---disable-indevs \
---disable-outdevs \
---disable-symver \
---enable-neon \
---enable-shared \
---disable-static \
---target-os=android
-
 # debian linux deploy
 
 PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure   --prefix="$HOME/ffmpeg_build"   --pkg-config-flags="--static"   --era-cflags="-I$HOME/ffmpeg_build/include"   --extra-ldflags="-L$HOME/ffmpeg_build/lib"   --extra-libs="-lpthread -lm"   --ld="g++"   --bindir="$HOME/bin"   --enable-gpl   --enable-gnutls   --enable-libfdk-aac   --enable-libfreetype   --enable-libvorbis   --enable-libvpx   --enable-libx264   --enable-libx265   --enable-nonfree --enable-cross-compile --disable-indevs --disable-outdevs --disable-symver --enable-neon --enable-shared --disable-static
-
