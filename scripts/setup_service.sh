6226a389542f5f0f7eecee95d8595a31342f7a36
André <eusoubrasileiro@gmail.com>
2021-12-04 21:15:50 -0300

    setup_service.sh fixing to chmod .py script

--- scripts/setup_service.sh
+++ scripts/setup_service.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # https://linuxconfig.org/how-to-run-script-on-startup-on-ubuntu-20-04-focal-fossa-server-desktop
-sudo chmod 744 ~/motion_server_nvr/run_motion_nvr.sh
-sudo cp ~/motion_server_nvr/pc_config/motion_nvr.service /etc/systemd/system/
+sudo chmod 744 ~/motion_server_nvr/motion_nvr.py
+sudo cp ~/motion_server_nvr/scripts/motion_nvr.service /etc/systemd/system/
 sudo chmod 664 /etc/systemd/system/motion_nvr.service
 sudo systemctl daemon-reload
 sudo systemctl enable motion_nvr
