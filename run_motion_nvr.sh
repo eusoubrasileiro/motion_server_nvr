ce7b02a68f3ecf496556db51484510831266937a
André <eusoubrasileiro@gmail.com>
2021-12-04 21:13:47 -0300

    simpler .service calling python script from venv directly

--- run_motion_nvr.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python3 /home/andre/motion_server_nvr/motion_nvr.py -d /mnt/motion_data -s /home/andre &> /dev/null # &> /dev/null mandatory to run as a service
--- setup_server.sh
+++ setup_server.sh
@@ -5,8 +5,8 @@ source ~/motion_server_nvr/ffmpeg_build/ffmpeg_install.sh
 source ~/motion_server_nvr/motion_build/motion_install.sh
 
 # https://linuxconfig.org/how-to-run-script-on-startup-on-ubuntu-20-04-focal-fossa-server-desktop
-sudo chmod 744 ~/motion_server_nvr/run_motion_nvr.sh
-sudo cp ~/motion_server_nvr/pc_config/motion_nvr.service /etc/systemd/system/
+sudo chmod 744 ~/motion_server_nvr/motion_nvr.py
+sudo cp ~/motion_server_nvr/scripts/motion_nvr.service /etc/systemd/system/
 sudo chmod 664 /etc/systemd/system/motion_nvr.service
 sudo systemctl daemon-reload
 sudo systemctl enable motion_nvr
