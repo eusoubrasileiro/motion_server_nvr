cf56a8c1954a4aac4fb3586beed1eae722b58ba0
André <eusoubrasileiro@gmail.com>
2021-12-09 19:53:07 -0300

    clone latest FFmpeg 4.4 and  apply a git .patch

--- ffmpeg_build/ffmpeg_install.sh
+++ ffmpeg_build/ffmpeg_install.sh
@@ -21,49 +21,27 @@ sudo apt-get update -qq && DEBIAN_FRONTEND=noninteractive sudo apt-get install -
   v4l2loopback-dkms # support hevc_qs intel hardware decoding h265
 # libv4l-dev libv4l-0  for video4linux decoders/encoders
 
-if [ ! -f "ffmpeg-4.3.zip" ]; then # only if not downloaded yet
-    wget -O ffmpeg-4.3.zip https://github.com/eusoubrasileiro/FFmpeg/archive/refs/heads/release/4.3.zip
+if [ ! -d "FFmpeg" ]; then # only if not cloned
+  git clone --depth 1 --branch release/4.4 https://github.com/FFmpeg/FFmpeg.git
 fi
-if [ ! -d "FFmpeg-release-4.3" ]; then # only if not unziped yet
-   unzip ffmpeg-4.3.zip
-fi
-    
 
-cd FFmpeg-release-4.3
-#export CFLAGS='-g -O3 -ftree-vectorize -mcpu=cortex-a53 -march=armv8-a+crypto+crc+simd' 
+cd FFmpeg
+git reset --hard 
+# Applying the patch: git apply --stat file.patch # show stats. 
+# git apply --check file.patch # check for error before applying. 
+# git am < file.patch # apply the patch finally.
+git config --global user.email aflopes7@gmail.com
+git config --global user.name eusoubrasileiro
+# apply the patch finally.
+git am <  ~/motion_server_nvr/ffmpeg_build/RTSP_LOWER_TRANSPORT_TCP-Yoose-Ip-Camera-Fix.path
+
 make clean 
 
-# based on ffmpeg-kit android.sh
-# compiling on a chroot linux android
-if [ "`uname -m`" = "aarch64" ] ; then  
-  ./configure --arch=arm64  --target-os=linux --enable-pic  --enable-optimizations  --enable-swscale \
-  --disable-outdevs    --disable-indevs  --disable-openssl  --disable-xmm-clobber-test  --disable-neon-clobber-test \
-  --disable-ffplay  --disable-postproc  --disable-doc  --disable-htmlpages  --disable-manpages  --disable-podpages \
-  --disable-txtpages   --disable-sndio  --disable-schannel --disable-securetransport  --disable-xlib  --disable-cuda  \
-  --disable-cuvid  --disable-nvenc   --disable-vaapi  --disable-vdpau  --disable-videotoolbox  --disable-audiotoolbox  \
-  --disable-appkit  --disable-alsa  --disable-cuda --disable-cuvid  --disable-nvenc  \
-  --disable-vaapi --disable-vdpau --enable-shared --enable-v4l2-m2m --prefix=/usr
-# --prefix=/usr/local is the default and binaries on /usr/local/bin
-# but better user /usr to avoid LD_LIBRARY_PATH set 
-# v4l2-m2m provides some hardware accelerated decoders/enconder
-fi
-#  compiling on real linux not android
+#  compiling on linux not android
 if [ "`uname -m`" = "x86_64" ] ; then 
-  # intel quick sync hardware decoders
-  sudo apt-get install libva-dev libmfx-dev intel-media-va-driver-non-free
-  export LIBVA_DRIVER_NAME=iHD
-
-  ./configure --enable-optimizations  --enable-swscale \
-  --disable-outdevs    --disable-indevs   \
+  ./configure --disable-outdevs  --disable-indevs   \
   --enable-shared --prefix=/usr/local --extra-libs="-lpthread -lm" \
-  --ld="g++" \
-  --enable-gpl \
-  --enable-gnutls \
-  --enable-nonfree \
-  --enable-libvorbis \
-  --arch=x86_64 --disable-yasm \
-  --enable-vaapi --enable-libmfx
-
+  --ld="g++" CFLAGS="-pthread"
   # --prefix=/usr/local is the default for real linux let it be
 fi
 
