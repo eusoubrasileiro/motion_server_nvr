053e3fe728c4affb01da073be668cf4def969855
eusoubrasileiro <eusoubrasileiro@gmail.com>
2022-06-17 09:55:25 -0300

    ffmpeg build a little faster, motion conf - trying faster ffmpeg build - motion .conf many errors trying fix

--- ansible/roles/jnano/files/ffmpeg_build.sh
+++ ansible/roles/jnano/files/ffmpeg_build.sh
@@ -13,10 +13,36 @@ git apply ../jetson-ffmpeg/ffmpeg_nvmpi.patch
 # Add my RTSP patch my Yoose trash cameras
 patch -p1 < ../RTSP_lower_transport_TCP.patch
 
+
 #  compiling jetson nano 
-./configure --disable-outdevs  --disable-indevs --enable-nvmpi \
+./configure --enable-nvmpi \
+--disable-outdevs  --disable-indevs --disable-devices \
+--disable-ffprobe --disable-ffplay \
 --enable-shared --prefix=/usr/local 
-#is the default for real linux let it be
+
+# ffmpeg build time reduced 10 fold (trying to)
+# - disabling almost everything possible 
+# - trying enable only what is needed 
+# - tested working
+# compiling using nvmpi (nvidia multimedia api) jetson nano hardware decoders
+# mpg2video, mpg4 is for mpg/mp4 timelapse videos encoded from 1 second jpegs
+# ./configure --enable-nvmpi --disable-outdevs  --disable-indevs  \
+# --disable-ffprobe --disable-ffplay --disable-encoders --disable-filters \
+# --disable-devices --disable-protocols --disable-decoders \
+# --disable-muxers --disable-demuxers \
+# --enable-protocol=rtmp \
+# --enable-encoder=mpeg2video --enable-encoder=mpeg4 \
+# --enable-muxer=mpeg2video --enable-muxer=mp4 \
+# --enable-decoder=hevc_nvmpi --enable-decoder=h264_nvmpi \
+# --enable-muxer=rtsp --enable-muxer=h264 --enable-muxer=hevc --enable-muxer=mp4 \
+# --enable-demuxer=rtsp --enable-demuxer=h264 --enable-demuxer=hevc  \
+# --enable-shared --prefix=/usr/local
+# more to add to try
+#--enable-demuxers=image_jpeg_pipe image2pipe image2
+#--enable-muxers=image2pipe
+
+
+
 # debbuging version
 #./configure --enable-shared --disable-static --disable-optimizations \
 #--disable-mmx --disable-stripping --enable-nvmpi --prefix=/usr/local
@@ -33,6 +59,7 @@ make -j$(nproc)
 # ldconfig
 
 # testing  hevc_nvmpi decoder 
-# ffmpeg -v quiet -stats -rtsp_transport tcp -y -c:v  hevc_nvmpi -i rtsp://user:pass@ipcam.kitchen:554/onvif2 -f null -
+# ffmpeg -v quiet -stats -rtsp_transport tcp -y -c:v  hevc_nvmpi -i rtsp://user:pass@ipcam-kitchen:554/onvif2 -f null -
 # Working perfectly after install. To show only progress use -v quiet -stats
 # less image smearing/tearing errors etc..  it seams
+ 
